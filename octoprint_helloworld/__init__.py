84b4b861c31bc9f7831c4ce41fbc356347832ad4
Zhiqi Lin <zhiqi.lin@smarp.com>
2016-11-25 23:16:10 +0200

    first commit

---
+++ octoprint_helloworld/__init__.py
@@ -0,0 +1,34 @@
+# coding=utf-8
+from __future__ import absolute_import
+
+import octoprint.plugin
+import logging
+
+class HelloWorldPlugin(octoprint.plugin.StartupPlugin,
+	octoprint.plugin.TemplatePlugin,
+	octoprint.plugin.SettingsPlugin,
+	octoprint.plugin.AssetPlugin):
+
+	def on_after_startup(self):
+		self._logger.info("Hello World! (more: %s)" % self._settings.get(["url"]))
+
+	def get_settings_defaults(self):
+		return dict(url="https://en.wikipedia.org/wiki/Hello_world")
+
+	def get_template_configs(self):
+		self._logger.info("My tag: inside HelloWorldPlugin.get_template_configs")
+		return [
+			dict(type="navbar", custom_bindings=False),
+			dict(type="settings", custom_bindings=False)
+		]
+
+	def get_assets(self):
+		self._logger.info("My tag: inside HelloWorldPlugin.get_assets")
+		return dict(
+			js=["js/helloworld.js"]
+		)
+
+#__plugin_name__ = "Hello World"
+
+__plugin_implementation__ = HelloWorldPlugin()
+
